d7a4318c97d0acc5cd4ed96a094161b5a4deffd2
Harrison Wilkinson <cole@0xcoast.com>
2024-05-17 08:23:42 +0200

    got get requests working

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 
 import './init-env.js'
 
-export const handleWeb = async (req, res) => {
+export const handleWebhook = async (req, res) => {
+    console.log('Received webhook:', req.method, req.url)
     if (req.method === 'GET') {
         console.log('Just a GET request')
         res.send('It is alive!')
         return
     }
+
+    const { body } = req
+
+    console.log('Received webhook:', body)
 }
--- init-env.js
+++ init-env.js
@@ -1,4 +1,3 @@
 import * as dotenv from 'dotenv'
 
-// This instantiates the local env file (it's copied directly from the Coast repo)
 dotenv.config()
